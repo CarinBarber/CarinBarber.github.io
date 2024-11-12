402415a06a7def65f29dd8c0c6bf957d9b765727
Carin Barber <cbarber2289@gmail.com>
2024-11-11 18:26:58 -0600

    checking carousel

--- site.js
+++ site.js
@@ -17,3 +17,39 @@ if(isMorning) {
     else {
         welcomeD.textContent = 'Good Evening'
     }
+
+
+
+
+const urls = [
+    'https://images.pexels.com/photos/1454360/pexels-photo-1454360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/933964/pexels-photo-933964.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/267885/pexels-photo-267885.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1251861/pexels-photo-1251861.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1370296/pexels-photo-1370296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+].map(url => { (new Image()).src = url; return url })
+    
+const images = document.querySelectorAll('#carousel img')
+    
+let currentImage = 0
+const showImages = () => {
+    const offset = currentImage % urls.length
+    images.forEach((image, index) => {
+        const imageIndex = (index + offset + urls.length) % urls.length
+        image.src = urls[imageIndex]
+        setInterval(() => { currentImage++, 5000 })
+    })
+    }
+    
+    showImages()
+
+const nextButton = document.querySelector('#next')
+    nextButton.addEventListener('click', () => {
+        currentImage++,
+        showImages()
+    })
+const prevButton = document.querySelector('#prev')
+    prevButton.addEventListener('click', () => {
+        currentImage--,
+        showImages()
+    }) 
